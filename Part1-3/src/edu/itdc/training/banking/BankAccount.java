780d193ead7d361bd9ba4a4c29a263297b35ace3
Xina-Torres <xina.torres@gmail.com>
2017-02-19 01:51:00 +0800

    Assigned values using Constructors, made BankAccount abstract

--- Part1-3/src/edu/itdc/training/banking/BankAccount.java
+++ Part1-3/src/edu/itdc/training/banking/BankAccount.java
@@ -1,19 +1,35 @@
 package edu.itdc.training.banking;
 
-public class BankAccount {
+public abstract class BankAccount {
 	private String accountNo;
 	private String accountName;
 	protected double balance;
 	public BankAccount(){}
 	public BankAccount(String accountNo, String accountName){
-		
+		this.accountNo = accountNo;
+		this.accountName = accountName;
 	}
 	public BankAccount(String accountNo, String accountName, double initBalance){
-		
+		this.accountNo = accountNo;
+		this.accountName = accountName;
+		this.balance = initBalance;
 	}
 	
-	public double getBalance(){return balance;}
+	
 
 	public boolean deposit(double amount){return false;}
 	public boolean withdraw(double amount){return false;}
+	
+	
+	public String getAccountName() {
+		return accountName;
+	}
+	
+	public String getAccountNo(){
+		return accountNo;
+	}
+	public double getBalance(){
+		return balance;
+	}
+
 }
