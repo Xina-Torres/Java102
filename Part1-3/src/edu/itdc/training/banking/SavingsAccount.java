ef4344372180dcc1bb42bf14c2b0589ef3e869c4
Xina-Torres <xina.torres@gmail.com>
2017-02-18 23:59:54 +0800

    Part 1 Step 5 Added super(accountNO, accountName)

--- Part1-3/src/edu/itdc/training/banking/SavingsAccount.java
+++ Part1-3/src/edu/itdc/training/banking/SavingsAccount.java
@@ -4,7 +4,7 @@ public class SavingsAccount extends BankAccount{
 	private double interestRate;
 	
 	public SavingsAccount(String accountNo, String accountName){
-		
+		super(accountNo, accountName);
 	}
 	
 	public SavingsAccount(String accountNo, String accountName, double initBalance, double interestRate){
