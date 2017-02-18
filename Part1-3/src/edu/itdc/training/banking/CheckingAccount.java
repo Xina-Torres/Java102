ae69cc480c2f4926115d60bd62bc4c77a4412e53
Xina-Torres <xina.torres@gmail.com>
2017-02-19 00:11:46 +0800

    Part 1 Step 9 Added super()

--- Part1-3/src/edu/itdc/training/banking/CheckingAccount.java
+++ Part1-3/src/edu/itdc/training/banking/CheckingAccount.java
@@ -3,7 +3,7 @@ package edu.itdc.training.banking;
 public class CheckingAccount extends BankAccount{
 	private double overdraftProtection;
 	public CheckingAccount(String accountNo, String accountName){
-		
+		super(accountNo, accountName);
 	}
 	
 	public CheckingAccount(String accountNo, String accountName, double initBalance, double overdraftProtection){
